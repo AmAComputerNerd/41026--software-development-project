04465e25cd98876bea9bef54023a0993dec1b377
AmAComputerNerd <thomsonjonathonw@outlook.com>
2026-08-17 22:12:16 +1000

    Seed 10 entities for each table instead of 5

--- student-3/backend/DeadlineTaskTracker/Api/Data/DbSeeder.cs
+++ student-3/backend/DeadlineTaskTracker/Api/Data/DbSeeder.cs
@@ -41,6 +41,31 @@ public class DbSeeder
                 {
                     Code = "31005",
                     Name = "Machine Learning"
+                },
+                new Course
+                {
+                    Code = "41080",
+                    Name = "Theory of Computing Science"
+                },
+                new Course
+                {
+                    Code = "48730",
+                    Name = "Cybersecurity"
+                },
+                new Course
+                {
+                    Code = "41052",
+                    Name = "Advanced Algorithms"
+                },
+                new Course
+                {
+                    Code = "33116",
+                    Name = "Design, Data, and Decisions"
+                },
+                new Course
+                {
+                    Code = "43030",
+                    Name = "Prof. Practice in Computing"
                 }
             );
 
@@ -56,6 +81,18 @@ public class DbSeeder
 
         if (!tasks.Any())
         {
+            var parentTask = new TaskEntity
+            {
+                Title = "Parent Task",
+                Description = "This is a seeded parent task.",
+                CourseId = courses.FirstOrDefault(c => c.Code == "43034")?.Id,
+                Priority = TaskPriority.High,
+                Status = TaskStatus.Todo,
+                DueDate = DateTimeOffset.UtcNow,
+                CreatedAt = DateTimeOffset.UtcNow,
+                UpdatedAt = DateTimeOffset.UtcNow
+            };
+            
             tasks.AddRange(
                 new TaskEntity
                 {
@@ -104,6 +141,54 @@ public class DbSeeder
                 {
                     Title = "Learning Machines",
                     Description = "AI :O",
+                    CourseId = courses.FirstOrDefault(c => c.Code == "31005")?.Id,
+                    Priority = TaskPriority.High,
+                    Status = TaskStatus.Completed,
+                    DueDate = DateTimeOffset.UtcNow,
+                    CreatedAt = DateTimeOffset.UtcNow,
+                    UpdatedAt = DateTimeOffset.UtcNow
+                },
+                parentTask,
+                new TaskEntity
+                {
+                    Title = "Child Task 1",
+                    Description = "This is a seeded child task.",
+                    CourseId = courses.FirstOrDefault(c => c.Code == "31005")?.Id,
+                    Priority = TaskPriority.High,
+                    Status = TaskStatus.Completed,
+                    DueDate = DateTimeOffset.UtcNow,
+                    CreatedAt = DateTimeOffset.UtcNow,
+                    UpdatedAt = DateTimeOffset.UtcNow,
+                    ParentTask = parentTask
+                },
+                new TaskEntity
+                {
+                    Title = "Child Task 2",
+                    Description = "This is a seeded child task.",
+                    CourseId = courses.FirstOrDefault(c => c.Code == "31005")?.Id,
+                    Priority = TaskPriority.Medium,
+                    Status = TaskStatus.InProgress,
+                    DueDate = DateTimeOffset.UtcNow,
+                    CreatedAt = DateTimeOffset.UtcNow,
+                    UpdatedAt = DateTimeOffset.UtcNow,
+                    ParentTask = parentTask
+                },
+                new TaskEntity
+                {
+                    Title = "Child Task 3",
+                    Description = "This is a seeded child task.",
+                    CourseId = courses.FirstOrDefault(c => c.Code == "31005")?.Id,
+                    Priority = TaskPriority.Low,
+                    Status = TaskStatus.Completed,
+                    DueDate = DateTimeOffset.UtcNow,
+                    CreatedAt = DateTimeOffset.UtcNow,
+                    UpdatedAt = DateTimeOffset.UtcNow,
+                    ParentTask = parentTask
+                },
+                new TaskEntity
+                {
+                    Title = "Parent Task with no Children",
+                    Description = "This is a seeded child task.",
                     CourseId = courses.FirstOrDefault(c => c.Code == "31005")?.Id,
                     Priority = TaskPriority.High,
                     Status = TaskStatus.Completed,
